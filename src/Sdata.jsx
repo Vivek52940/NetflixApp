fca279b32e0469b068cfc4bf69e13efc498728c1
vivek singh <imvsingh64@gmail.com>
2020-06-30 10:16:47 +0530

    initial phase

---
+++ src/Sdata.jsx
@@ -0,0 +1,23 @@
+const Sdata = [
+    {
+        sname: "DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80990668?source=35",
+        
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"A Netflix Original Series",
+        sname:"Stranger Things",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://images.justwatch.com/poster/8589251/s592",
+        title:"A Netflix Original Series",
+        sname:"The Vampire Diaries",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    }
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import  Card from "./Card.jsx"
+import "./index.css";
+import Sdata from "./Sdata.jsx";
+//import Sdata from "./Sdata";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<h1 className="Heading">List of Top 3 Netflix Series in 2020</h1>
+<Card
+sname= {Sdata[0].sname}
+imgsrc={Sdata[0].imgsrc}
+title={Sdata[0].title}
+link={Sdata[0].link}/>
+<Card
+sname= {Sdata[1].sname}
+imgsrc={Sdata[1].imgsrc}
+title={Sdata[1].title}
+link={Sdata[1].link}/>
+<Card
+sname= {Sdata[2].sname}
+imgsrc={Sdata[2].imgsrc}
+title={Sdata[2].title}
+link={Sdata[2].link}/>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+</>
+  , document.getElementById("root")
+)
