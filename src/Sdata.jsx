5c926e260789f8f797329a1af8c18869c50e931b
vivek singh <imvsingh64@gmail.com>
2020-06-30 13:34:46 +0530

    final change

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -7,15 +7,15 @@ const Sdata = [
         
     },
     {
+        sname:"Stranger Things",
         imgsrc:"https://wallpapercave.com/wp/wp1917154.jpg",
         title:"A Netflix Original Series",
-        sname:"Stranger Things",
         link:"https://www.netflix.com/in/title/80990668?source=35"
     },
     {
+        sname:"The Vampire Diaries",
         imgsrc:"https://images.justwatch.com/poster/8589251/s592",
         title:"A Netflix Original Series",
-        sname:"The Vampire Diaries",
         link:"https://www.netflix.com/in/title/80990668?source=35"
     }
 ]
--- src/index.js
+++ src/index.js
@@ -4,24 +4,27 @@ import  Card from "./Card.jsx"
 import "./index.css";
 import Sdata from "./Sdata.jsx";
 //import Sdata from "./Sdata";
+
+function ncard(val){
+  console.log(val);
+  return (
+    <Card
+     imgsrc={val.imgsrc}
+     title={val.title}
+     sname={val.sname}
+     link={val.links}
+     />
+  );
+}
 ReactDOM.render(
 <>
 <h1 className="Heading">List of Top 3 Netflix Series in 2020</h1>
-<Card
+{/*<Card
 sname= {Sdata[0].sname}
 imgsrc={Sdata[0].imgsrc}
 title={Sdata[0].title}
-link={Sdata[0].link}/>
-<Card
-sname= {Sdata[1].sname}
-imgsrc={Sdata[1].imgsrc}
-title={Sdata[1].title}
-link={Sdata[1].link}/>
-<Card
-sname= {Sdata[2].sname}
-imgsrc={Sdata[2].imgsrc}
-title={Sdata[2].title}
-link={Sdata[2].link}/>
+link={Sdata[0].link}/>*/}
+{Sdata.map(ncard)};
 
 </>
   , document.getElementById("root")
